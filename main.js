05c102b380cab48426db246b150b4813d8cee607
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 19:20:25 +0100

    ett exampel om slice med start och end

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 /*
 Split : split( separator , limit )
+Slice : slice( Start, End )
 */
 
 /*
@@ -73,10 +74,10 @@ console.log(mySplittedString);
 
 console.log(typeof(mySplittedString));
 */
-
+/*
 var myMainString = " I Love JavaScript And JSON",
 
-mySplittedString = myMainString.split(" ");
+mySplittedString = myMainString.split(" ", 4);
 
 console.log(myMainString);
 
@@ -86,6 +87,82 @@ console.log(typeof(myMainString));
 console.log(mySplittedString);
 
 console.log(typeof(mySplittedString));
+*/
+
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split(" ", 2);
+
+console.log(myMainString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySplittedString);
+
+console.log(typeof(mySplittedString));
+*/
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySlicedString = myMainString.slice();
+
+console.log(mySlicedString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySlicedString);
+
+console.log(typeof(mySplittedString));
+*/
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySlicedString = myMainString.slice(5);
+
+console.log(mySlicedString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySlicedString);
+
+console.log(typeof(mySlicedString));
+*/
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySlicedString = myMainString.slice(5, 10);
+
+console.log(mySlicedString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySlicedString);
+
+console.log(typeof(mySlicedString));
+*/
+
+
+var myMainString = " I Love JavaScript And JSON",
+
+mySlicedString = myMainString.slice(1, 10);
+
+console.log(mySlicedString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySlicedString);
+
+console.log(typeof(mySlicedString));
+
+
+
+
 
 
 
