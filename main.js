01b33f62bc179c2965cf19d97a8c6ea581c7b555
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 19:13:25 +0100

    träning om ett exampel som giller separator

---
+++ main.js
@@ -0,0 +1,92 @@
+/* global console,alert,prompt*/
+
+/*
+Split : split( separator , limit )
+*/
+
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split();
+
+console.log(mySplittedString);
+*/
+
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split();
+
+console.log(mySplittedString);
+
+console.log(myMainString);
+*/
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split();
+
+console.log(myMainString);
+
+console.log(mySplittedString);
+*/
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split();
+
+console.log(myMainString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySplittedString);
+
+console.log(typeof(mySplittedString));
+*/
+
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split("");
+
+console.log(myMainString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySplittedString);
+
+console.log(typeof(mySplittedString));
+*/
+/*
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split(" ");
+
+console.log(myMainString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySplittedString);
+
+console.log(typeof(mySplittedString));
+*/
+
+var myMainString = " I Love JavaScript And JSON",
+
+mySplittedString = myMainString.split(" ");
+
+console.log(myMainString);
+
+console.log(typeof(myMainString));
+
+
+console.log(mySplittedString);
+
+console.log(typeof(mySplittedString));
+
+
+
+
